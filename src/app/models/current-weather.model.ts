fef94ae9a3d3631dea40b4d8af59ddb957bdc635
sundarcodes <sundar82@gmail.com>
2017-03-19 10:19:46 +0530

    1. Added more methods in weather service 2. Added current weather model

---
+++ src/app/models/current-weather.model.ts
@@ -0,0 +1,11 @@
+export class WeatherToday {
+  dateTime: number;
+  avgTemp: number;
+  howItFeelsLikeNow: string;
+  humidity: number;
+  windSpeed: number;
+  maxTemp: number;
+  minTemp: number;
+  howIsTheDayLike: string;
+  hourlyForecast: number[];
+}
--- src/app/providers/weather.service.ts
+++ src/app/providers/weather.service.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { Observable, BehaviorSubject } from 'rxjs';
+
+import { WeatherToday } from '../models/current-weather.model';
 
 @Injectable()
 export class WeatherService {
 
-  constructor() { }
+  private baseUrl: string;
+  private weatherTodaySub : BehaviorSubject<WeatherToday>;
+  private weatherToday: WeatherToday;
+  public weatherToday$: Observable<WeatherToday>;
+  private dayWiseMap: any;
+
+  constructor(private _http: Http) {
+    this.baseUrl = 'http://api.openweathermap.org/data/2.5/forecast?appid=27d43832d2a4adcb97fcbfa23db130aa&q=London,us';
+    this.weatherToday = new WeatherToday();
+    this.weatherTodaySub = new BehaviorSubject(this.weatherToday);
+    this.weatherToday$ = this.weatherTodaySub.asObservable();
+  }
+
+  sendRequestForCity(city: string): Observable<any> {
+    const completeURL: string = this.baseUrl + city;
+    return this._http.get(completeURL)
+    .do(x => console.log(x))
+    .map((data: any) => data.list);
+  }
+
+  loadWeatherInfoForCity(city: string): Observable<any> {
+    return this.sendRequestForCity(city)
+    .switchMap(forecastList => this.buildDayWiseMap(forecastList));
+  }
+
+  buildDayWiseMap(forecastList) {
+    return [];
+  }
+
+
+
+
 
 }
